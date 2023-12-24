0380f8aaa6b9990c1be7ba8d29733c8d9dd9fb0d
Stanislav <akstas84@users.noreply.github.com>
2023-12-24 10:34:11 +0300

    first commit

---
+++ src/main/java/pro/sky/skyspringbook/SkySpringBookApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyspringbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkySpringBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkySpringBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyspringbook/SkySpringBookApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyspringbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkySpringBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
